b108ba87773fe579a969150ee4ae41ef7fe5f238
od3ng <noprianto@polinema.ac.id>
2019-08-11 23:59:09 +0700

    1. fixing import

--- src/main/java/com/sinaungoding/web/consumeapi/controller/MahasiswaController.java
+++ src/main/java/com/sinaungoding/web/consumeapi/controller/MahasiswaController.java
@@ -4,7 +4,6 @@ import com.sinaungoding.web.consumeapi.dto.Mahasiswa;
 import com.sinaungoding.web.consumeapi.service.MahasiswaRestClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
